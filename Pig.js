e648ea703d4ae1f8108ed249720533ec03658de6
oodhav <80772330+oodhav@users.noreply.github.com>
2021-07-03 15:48:39 +0530

    Update Pig.js

--- Pig.js
+++ Pig.js
@@ -2,7 +2,7 @@ class Pig {
     constructor(x, y) {
       var options = {
           'restitution':0.8,
-          'friction':1.0,
+          'friction':2.0,
           'density':1.0
       }
       this.body = Bodies.rectangle(x, y, 50, 50, options);
