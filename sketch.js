d2d8535b3f01ff32ecfac2f22b1b98ae491028cf
oodhav <80772330+oodhav@users.noreply.github.com>
2021-07-03 15:47:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,7 +26,7 @@ function setup(){
 
     box5 = new Box(810,160,70,70)
     log3 = new Log(760,120,150,PI/7)
-    log4 = new Log(870,120,150,PI/7)
+    log4 = new Log(870,120,150,-PI/7)
 
     bird1 = new Bird(200,200)
 }
